f1a092657a234bf3c5e94109a6d195634f498a13
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-12-20 17:37:07 -0800

    added in carousel

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { images } from '../images';
+import { Gallery, GalleryImage } from 'react-gesture-gallery';
 
-export default class About extends Component {
-    render() {
+
+const INITITAL = 0;
+
+export default function About() {
+    
+        const [index, setIndex ] = React.useState(INITITAL);
+
+        React.useEffect(() => {
+           const interval = setInterval(() => {
+               if (index === images.length - 1) {
+                   setIndex(INITITAL)
+               } 
+               else {
+                   setIndex(index + 1)
+               }
+               setIndex(index + 1)
+            }, 30000)
+           return () => clearInterval(interval)
+        }, [index])
+
         return (
-            <div>
-                <h1>Hi from About</h1>
-            </div>
+            <Gallery style={{height: '100vh', width: '100vw', background: 'black'}} index={index} onRequestChange={i => { setIndex(i)}}>
+                {images.map(image => (
+                    <GalleryImage objectFit="contain" src={image} /> 
+                ))}
+            </Gallery>
         )
-    }
+
 }
---
+++ src/images.js
@@ -0,0 +1,8 @@
+export const images = [
+"https://images.unsplash.com/photo-1576193929684-06c6c6a8b582?ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=80",
+"https://images.unsplash.com/photo-1575633596232-b768665ea397?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3334&q=80",
+"https://images.unsplash.com/photo-1574656722029-7ac81203dce4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1301&q=80",
+"https://images.unsplash.com/photo-1574388548306-1e8e7aea8be4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+"https://images.unsplash.com/photo-1574392150428-6833474a91b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1258&q=80",
+"https://images.unsplash.com/flagged/photo-1574368823068-f5cebefda09d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1257&q=80"
+];
