25df0c61d4c5d3024c4bd448394c54323075cc33
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-12-22 15:36:43 -0800

    added in ml technologies

--- src/components/Other1.js
+++ src/components/Other1.js
@@ -7,35 +7,35 @@ const movies = [
       id: 1,
       image: '/images/angularPic.jpg',
       imageBg: '/images/angularPic.jpg',
-      title: 'FOREST',
+      title: 'Angular',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #1 but will test 3 to see if it works!'
     },
     {
       id: 2,
       image: '/images/reactPic.jpg',
       imageBg: '/images/reactPic.jpg',
-      title: 'STUff',
+      title: 'React',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #2 but will test 3 to see if it works!'
     },
     {
       id: 3,
       image: '/images/djangoPic.jpg',
       imageBg: '/images/djangoPic.jpg',
-      title: 'The Other stuff',
+      title: 'Django',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #3 but will test 3 to see if it works!'
     },
     {
       id: 4,
       image: '/images/expressPic.jpg',
       imageBg: '/images/expressPic.jpg',
-      title: 'Blah blah...',
+      title: 'Express',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #4 but will test 3 to see if it works!'
     },
     {
       id: 5,
       image: '/images/meteorPic.jpg',
       imageBg: '/images/meteorPic.jpg',
-      title: 'Blah blah...',
+      title: 'Meteor',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #5 but will test 3 to see if it works!'
 
     },
@@ -43,35 +43,35 @@ const movies = [
       id: 6,
       image: '/images/rorPic.png',
       imageBg: '/images/rorPic.png',
-      title: 'Blah blah...',
+      title: 'Ruby on Rails',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #6 but will test 3 to see if it works!'
     },
     {
       id: 7,
       image: '/images/springPic.jpg',
       imageBg: '/images/springPic.jpg',
-      title: 'Blah blah...',
+      title: 'Spring',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #7 but will test 3 to see if it works!'
     },
     {
       id: 8,
       image: '/images/vuePic.jpg',
       imageBg: '/images/vuePic.jpg',
-      title: 'Blah blah...',
+      title: 'Vue',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #8 but will test 3 to see if it works!'
     },
     {
       id: 9,
       image: '/images/asapPic.jpg',
       imageBg: '/images/asapPic.jpg',
-      title: 'Blah blah...',
+      title: 'ASAP.NET',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #9 but will test 3 to see if it works!'
     },
     {
       id: 10,
       image: '/images/emberPic.png',
       imageBg: '/images/emberPic.png',
-      title: 'Blah blah...',
+      title: 'Emberp',
       description : 'This is a test to see if individual content works, as of right now this content is just for the ID #10 but will test 3 to see if it works!'
     }
   ];
@@ -79,62 +79,62 @@ const movies = [
   const movies1 = [
     {
       id: 1,
-      image: '/images/slide2.jpg',
-      imageBg: '/images/slide2.jpg',
-      title: 'FOREST'
+      image: '/images/tensorflowML.jpg',
+      imageBg: '/images/tensorflowML.jpg',
+      title: 'Tensor Flow'
     },
     {
       id: 2,
-      image: '/images/slide2.jpg',
-      imageBg: '/images/slide2.jpg',
-      title: 'STUff'
+      image: '/images/microsoftML.jpg',
+      imageBg: '/images/microsoftML.jpg',
+      title: 'Microsoft CNTK (Cognitive Toolkit)'
     },
     {
       id: 3,
-      image: '/images/slide2.jpg',
-      imageBg: '/images/slide2.jpg',
+      image: '/images/pythonML.jpg',
+      imageBg: '/images/pythonML.jpg',
       title: 'The Other stuff',
     },
     {
       id: 4,
-      image: '/images/slide5.jpg',
-      imageBg: '/images/slide5.jpg',
+      image: '/images/mML.svg',
+      imageBg: '/images/mML.svg',
       title: 'Blah blah...'
     },
     {
       id: 5,
-      image: '/images/slide5.jpg',
-      imageBg: '/images/slide5.jpg',
+      image: '/images/apachesparkML.jpg',
+      imageBg: '/images/apachesparkML.jpg',
       title: 'Blah blah...'
     },
     {
       id: 6,
-      image: '/images/slide1.jpg',
-      imageBg: '/images/slide1.jpg',
+      image: '/images/amazonML.jpg',
+      imageBg: '/images/amazonML.jpg',
       title: 'Blah blah...'
     },
     {
       id: 7,
-      image: '/images/slide2.jpg',
-      imageBg: '/images/slide2.jpg',
+      image: '/images/pytorchML.jpg',
+      imageBg: '/images/pytorchML.jpg',
       title: 'Blah blah...',
     },
     {
       id: 8,
-      image: '/images/slide3.jpg',
-      imageBg: '/images/slide3.jpg',
+      image: '/images/caffe2ML.jpg',
+      imageBg: '/images/caffe2ML.jpg',
       title: 'Blah blah...'
     },
     {
       id: 9,
-      image: '/images/slide4.jpg',
-      imageBg: '/images/slide4.jpg',
+      image: '/images/apacheSingmaML.jpg',
+      imageBg: '/images/apacheSingmaML.jpg',
       title: 'Blah blah...'
     },
     {
       id: 10,
-      image: '/images/slide5.jpg',
-      imageBg: '/images/slide5.jpg',
+      image: '/images/accordNetML.jpg',
+      imageBg: '/images/accordNetML.jpg',
       title: 'Blah blah...'
     }
   ];
