813b44cddd3d21fd884e5eb8983d75e02fbb04f5
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-01-27 19:38:54 -0800

    reverted old styles

--- src/components/Frontend.js
+++ src/components/Frontend.js
@@ -1,58 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "../components/Frontend.css";
 
 export default class Frontend extends Component {
-    render() {
-        return (
-            <div>
-                <div id="title" class="slide header">
-  <h1>Front End</h1>
-</div>
-
-<div id="slide1" class="slide">
-  <div class="title">
-    <h1>Slide 1</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
-  </div>
-</div>
-
-<div id="slide2" class="slide">
-  <div class="title">
-    <h1>Slide 2</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
-  </div>
-  <img className="post-card" src="" alt="parallax"/>
-  <img className="post-card" src="" alt="parallax" /> 
-</div>
-
-<div id="slide3" class="slide">
-  <div class="title">
-    <h1>Slide 3</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
-  </div>
-</div>
-
-<div id="slide2" class="slide">
-  <div class="title">
-    <h1>Slide 4</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
-  </div>
-  <img className="post-card" src="" alt="parallax" />
-  <img className="post-card" src="" alt="parallax" /> 
-</div>
-
-<div id="slide3" class="slide">
-  <div class="title">
-    <h1>Slide 5</h1>
-    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
-  </div>
-</div>
-
-
-<div id="slide4" class="slide header">
-    <h1>Footer</h1>
-</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div id="title" class="slide header">
+          <h1>Front End</h1>
+        </div>
+      </div>
+    );
+  }
 }
