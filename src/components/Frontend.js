11c0bfbb2b7fbec615da9338b4b85a9d6144b5c3
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-02-16 14:03:35 -0800

    finished frontend details

--- src/components/Frontend.js
+++ src/components/Frontend.js
@@ -57,7 +57,7 @@ export default class Frontend extends Component {
 						<div class="pricing-item-box">
 							<span class="ribbon"></span>
 						<div class="plan-name">React.js</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Agencies who want a brandable marketing platform</div>
+						<div class="plan-text"><span class="text-bold">Best for</span> makes user interfaces very easy to build by cutting each page into pieces</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros<span class="month-text"> / of React.js</span></div>
@@ -65,9 +65,12 @@ export default class Frontend extends Component {
 						</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">30 Clients</div>
-							<div class="plan-feature-item">Unlimited Reports</div>
-							<div class="plan-feature-item">Unlimited Live Dashboards</div>
+							<div class="plan-feature-item">Easy to learn and use</div>
+							<div class="plan-feature-item">Reuseable components</div>
+							<div class="plan-feature-item">Performance enchancement</div>
+							<div class="plan-feature-item">Benifits with Javascript</div>
+							<div class="plan-feature-item">Handy support tools</div>
+							<div class="plan-feature-item">Widely used framework</div>
 						</div>
 						<div class="plan-button"><a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" class="plan-button-blue ">React.js</a></div>
 						</div>
@@ -75,7 +78,7 @@ export default class Frontend extends Component {
 					<div class="pricing-item">
 						<div class="pricing-item-box">
 						<div class="plan-name">Fundamentals</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Large agencies who want to scale their processes</div>
+						<div class="plan-text"><span class="text-bold">Best to</span> learn starting from the basics and building of what you've learned</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros of the Basics</div>
@@ -83,15 +86,14 @@ export default class Frontend extends Component {
 						</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">Everything in Team plan &</div>
-							<div class="plan-feature-item">Marketing Operations - Workflow Management</div>
-							<div class="plan-feature-item">Marketing Analytics</div>
-							<div class="plan-feature-item">Custom Connectors</div>
-							<div class="plan-feature-item">Data </div>
-							<div class="plan-feature-item">HIPAA compliance</div>
-							<div class="plan-feature-item">Franchise Solutions</div>
-							<div class="plan-feature-item">Storage</div>
-							<div class="plan-feature-item">150+ Data Sources</div>
+							<div class="plan-feature-item">HTML is easy to learn and use</div>
+							<div class="plan-feature-item">Its free</div>
+							<div class="plan-feature-item">Simple to edit</div>
+							<div class="plan-feature-item">Intergrate easily with other languages</div>
+							<div class="plan-feature-item">Basics of programming language</div>
+							<div class="plan-feature-item">displays changes instantly</div>
+							<div class="plan-feature-item">HTML is user friendly</div>
+							
 						</div>
 						<div class="plan-button"><a href="/">The Basics</a></div>
 						</div>
