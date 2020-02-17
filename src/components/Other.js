ec336841e9e0559c21ef92b6e41b017ca40df50f
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-02-17 14:28:14 -0800

    changed backend framerwork details

--- src/components/Other.js
+++ src/components/Other.js
@@ -15,7 +15,7 @@ export default class Other extends Component {
 					<div class="pricing-item">
 						<div class="pricing-item-box">
 						<div class="plan-name">Larvel</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Startup agencies who want to automate their reporting</div>
+						<div class="plan-text"><span class="text-bold">Best for</span> the right tools to help build websites and web apps faster, more stable</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros<span class="month-text"> / of Larvel</span></div>
@@ -23,8 +23,10 @@ export default class Other extends Component {
 						</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">5 Clients</div>
-							
+							<div class="plan-feature-item">makes implementing authentication very simple</div>
+							<div class="plan-feature-item">Laravel provides a clean, simple API</div>
+							<div class="plan-feature-item">Laravel supports popular cache backends</div>
+							<div class="plan-feature-item">Laravel helps to secure the web application</div>
 						</div>
 
 						<div class="plan-button"><a href="https://laravel.com/" target="_blank" rel="noopener noreferrer">Larvel</a></div>
@@ -33,16 +35,19 @@ export default class Other extends Component {
 					<div class="pricing-item">
 						<div class="pricing-item-box">
 						<div class="plan-name">Django</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Startup agencies who want to automate their reporting</div>
+						<div class="plan-text"><span class="text-bold">Best for</span> rapid-development, fast processing, scalability</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros<span class="month-text"> / of Django</span></div>
 												</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">15 Clients</div>
-							<div class="plan-feature-item">Unlimited Reports</div>
-							<div class="plan-feature-item">Unlimited Live Dashboards</div>
+							<div class="plan-feature-item">Implemented in Python</div>
+							<div class="plan-feature-item">Better CDN connectivity and Content Management</div>
+							<div class="plan-feature-item">Fast Processing</div>
+							<div class="plan-feature-item">Offers Rapid-development</div>
+							<div class="plan-feature-item">Scalable</div>
+							<div class="plan-feature-item">Security</div>
 						</div>
 
 						<div class="plan-button"><a href="https://www.djangoproject.com/" target="_blank" rel="noopener noreferrer">Django</a></div>
@@ -53,7 +58,7 @@ export default class Other extends Component {
 						<div class="pricing-item-box">
 							<span class="ribbon"></span>
 						<div class="plan-name">Express</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Agencies who want a brandable marketing platform</div>
+						<div class="plan-text"><span class="text-bold">Best for</span> Insanely fast/small server-side JavaScript web development framework built on node.js</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros<span class="month-text"> / of Express</span></div>
@@ -61,9 +66,11 @@ export default class Other extends Component {
 						</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">30 Clients</div>
-							<div class="plan-feature-item">Unlimited Reports</div>
-							<div class="plan-feature-item">Unlimited Live Dashboards</div>
+							<div class="plan-feature-item">Easy to learn</div>
+							<div class="plan-feature-item">Fast app development</div>
+							<div class="plan-feature-item">I/O request handling</div>
+							<div class="plan-feature-item">Open-source community</div>
+							<div class="plan-feature-item">Easy integration of third-party services</div>
 						</div>
 						<div class="plan-button"><a href="https://expressjs.com/" target="_blank" rel="noopener noreferrer" class="plan-button-blue ">Express</a></div>
 						</div>
@@ -71,7 +78,7 @@ export default class Other extends Component {
 					<div class="pricing-item">
 						<div class="pricing-item-box">
 						<div class="plan-name">Ruby on Rails</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Large agencies who want to scale their processes</div>
+						<div class="plan-text"><span class="text-bold">Best for</span> offering such as an ORM system for business data and logic,routing,and application management</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros of Ruby on Rails</div>
@@ -79,15 +86,9 @@ export default class Other extends Component {
 						</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">Everything in Team plan &</div>
-							<div class="plan-feature-item">Marketing Operations - Workflow Management</div>
-							<div class="plan-feature-item">Marketing Analytics</div>
-							<div class="plan-feature-item">Custom Connectors</div>
-							<div class="plan-feature-item">Data </div>
-							<div class="plan-feature-item">HIPAA compliance</div>
-							<div class="plan-feature-item">Franchise Solutions</div>
-							<div class="plan-feature-item">Storage</div>
-							<div class="plan-feature-item">150+ Data Sources</div>
+							<div class="plan-feature-item">Best Industry Standards</div>
+							<div class="plan-feature-item"> Speed of Development</div>
+							<div class="plan-feature-item">Vibrant RoR Community</div>
 						</div>
 						<div class="plan-button"><a href="https://rubyonrails.org/">Ruby on Rails</a></div>
 						</div>
