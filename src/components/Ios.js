a428612899ff3c322382e1ffddf63597e88eadc6
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-02-18 17:05:57 -0800

    added in mobile details

--- src/components/Ios.js
+++ src/components/Ios.js
@@ -33,16 +33,17 @@ export default class Ios extends Component {
 					<div class="pricing-item">
 						<div class="pricing-item-box">
 						<div class="plan-name">Apache Cordova</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Startup agencies who want to automate their reporting</div>
+						<div class="plan-text"><span class="text-bold">Best for</span> use standard web technologies - HTML5, CSS3, and JavaScript for cross-platform development</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros<span class="month-text"> / of Apache Cordova</span></div>
 												</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">15 Clients</div>
-							<div class="plan-feature-item">Unlimited Reports</div>
-							<div class="plan-feature-item">Unlimited Live Dashboards</div>
+							<div class="plan-feature-item">create cross-platform mobile applications</div>
+							<div class="plan-feature-item">Saves time as hybrid apps</div>
+							<div class="plan-feature-item">Easier to maintain</div>
+							<div class="plan-feature-item">Cost-effective as you don’t have to spend resources</div>
 						</div>
 
 						<div class="plan-button"><a href="https://cordova.apache.org/" target="_blank" rel="noopener noreferrer">Apache Cordova</a></div>
@@ -53,7 +54,7 @@ export default class Ios extends Component {
 						<div class="pricing-item-box">
 							<span class="ribbon"></span>
 						<div class="plan-name">React Native</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Agencies who want a brandable marketing platform</div>
+						<div class="plan-text"><span class="text-bold">Best for</span> the best parts of native development with React</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros<span class="month-text"> / of React Native</span></div>
@@ -61,9 +62,12 @@ export default class Ios extends Component {
 						</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">30 Clients</div>
-							<div class="plan-feature-item">Unlimited Reports</div>
-							<div class="plan-feature-item">Unlimited Live Dashboards</div>
+							<div class="plan-feature-item">a best-in-class JavaScript library</div>
+							<div class="plan-feature-item">Faster to Build</div>
+							<div class="plan-feature-item">One Framework, Multiple Platforms</div>
+							<div class="plan-feature-item">Hot Reloading</div>
+							<div class="plan-feature-item">Fast Applications.</div>
+							<div class="plan-feature-item">Simplified UI.</div>
 						</div>
 						<div class="plan-button"><a href="https://facebook.github.io/react-native/" target="_blank" rel="noopener noreferrer" class="plan-button-blue">React Native</a></div>
 						</div>
@@ -71,7 +75,7 @@ export default class Ios extends Component {
 					<div class="pricing-item">
 						<div class="pricing-item-box">
 						<div class="plan-name">Ionic</div>
-						<div class="plan-text"><span class="text-bold">Best for</span> Something</div>
+						<div class="plan-text"><span class="text-bold">Best for</span> all types of apps with one shared code base</div>
 
 						<div class="plan-price-content">
 							<div class="plan-price">Pros of Ionic</div>
@@ -79,15 +83,13 @@ export default class Ios extends Component {
 						</div>
 
 						<div class="plan-features">
-							<div class="plan-feature-item">Everything in Team plan &</div>
-							<div class="plan-feature-item">Marketing Operations - Workflow Management</div>
-							<div class="plan-feature-item">Marketing Analytics</div>
-							<div class="plan-feature-item">Custom Connectors</div>
-							<div class="plan-feature-item">Data </div>
-							<div class="plan-feature-item">HIPAA compliance</div>
-							<div class="plan-feature-item">Franchise Solutions</div>
-							<div class="plan-feature-item">Storage</div>
-							<div class="plan-feature-item">150+ Data Sources</div>
+							<div class="plan-feature-item">Beautiful, reusable components</div>
+							<div class="plan-feature-item">Write once, run anywhere</div>
+							<div class="plan-feature-item">Developer friendly tooling</div>
+							<div class="plan-feature-item">New UI components and gestures</div>
+							<div class="plan-feature-item">Brand new custom animations API</div>
+							<div class="plan-feature-item">New icons, colors, and theming</div>
+							<div class="plan-feature-item">React & Angular integrations</div>
 						</div>
 						<div class="plan-button"><a href="https://ionicframework.com/" target="_blank" rel="noopener noreferrer">Ionic</a></div>
 						</div>
