232eb887eccd26de259bbd5c7af6f8aee699bcd3
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-02-25 19:30:46 -0800

    added in divs for styling

--- src/components/Other2.js
+++ src/components/Other2.js
@@ -10,7 +10,11 @@ export default class Other2 extends Component {
 
         <div className="game-main-div">
           <div className="game-double-div">
-            <div className="game-div-one" style={{backgroundImage:'url(https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2244&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>div1</div>
+            <div className="game-div-one" style={{backgroundImage:'url(https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2244&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', padding: '190px', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
+            div1
+            </div>
+            </div>
             <div className="game-div-two" style={{backgroundImage:'url(https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>div2</div>
           </div>
           <div className="game-div-three" style={{backgroundImage:'url(https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2309&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>div3</div>
