fab9870a490aaaa29225d710660d163f80d3b0da
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-03-29 22:08:59 -0700

    added in new content

--- src/components/Other2.js
+++ src/components/Other2.js
@@ -51,19 +51,19 @@ export default class Other2 extends Component {
         <div className="game-main-div">
           <div className="game-double-div">
             <div className="game-div-one" style={{backgroundImage:'url(https://images.unsplash.com/photo-1573487903232-46df3657c783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', padding: '190px', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
-            Testing content for div 7
+            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', height:"87%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
+            <p style={{color: 'white', padding: '20px'}}>Testing content for div 7</p>
             </div>
             </div>
             <div className="game-div-two" style={{backgroundImage:'url(https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', padding: '120px 200px', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
-            Testing content for div 8
+            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
+            <p style={{color: 'white', padding: '20px'}}>Testing content for div 8</p>
             </div>
             </div>
           </div>
           <div className="game-div-three" style={{backgroundImage:'url(https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-          <div style={{backgroundColor: 'rgba(30,30,30,0.7)', padding: '400px 300px', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
-          Testing content for div 9
+          <div style={{backgroundColor: 'rgba(30,30,30,0.7)',height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
+          <p style={{color: 'white', padding: '20px'}}>Testing content for div 9</p>
           </div>
           </div>
         </div>
