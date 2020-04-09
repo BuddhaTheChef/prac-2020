e0eaab9aec28f6ac77527639302cd6597db0bded
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-04-09 14:35:05 -0700

    final div for gb page

--- src/components/Other2.js
+++ src/components/Other2.js
@@ -70,7 +70,7 @@ export default class Other2 extends Component {
 
            <div className="game-main-div-rev">
           <div className="game-div-three" style={{backgroundImage:'url(https://images.unsplash.com/photo-1506808547685-e2ba962ded60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-          <div style={{backgroundColor: 'rgba(30,30,30,0.7)',height:"87%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
+          <div style={{backgroundColor: 'rgba(30,30,30,0.7)', marginTop: "19px", height:"90%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
           <p style={{color: 'white', padding: '20px'}}>Testing content for div 4</p>
           </div>
           </div>
