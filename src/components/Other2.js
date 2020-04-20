579e4028aacfa1cb951f1ca3dc47f0a5421a2efa
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-04-20 11:32:28 -0700

    changed styles

--- src/components/Other2.js
+++ src/components/Other2.js
@@ -11,32 +11,32 @@ export default class Other2 extends Component {
         <div className="game-main-div">
           <div className="game-double-div">
             <div className="game-div-one" style={{backgroundImage:'url(https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2244&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', height:"80%",width:'80%', borderRadius: '5px', border: 'solid 3px white'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.9)', height:"80%",width:'80%', borderRadius: '5px', border: 'solid 3px white'}}>
             <p style={{color: 'white', padding: '20px'}}>Unity</p>
             <p style={{color: 'white', padding: '20px'}}>Unreal Engine</p>
             <p style={{color: 'white', padding: '20px'}}>Corona SDK</p>
             </div>
             </div>
             <div className="game-div-two" style={{backgroundImage:'url(https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', height: '80%', width: '80%', color: 'white', borderRadius: '5px', border: 'solid 3px white', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.9)', height: '80%', width: '80%', color: 'white', borderRadius: '5px', border: 'solid 3px white', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <p style={{color: 'white', padding: '20px'}}>Mobile Gaming Technologies</p>
             </div>
             </div>
           </div>
           <div className="game-div-three" style={{backgroundImage:'url(https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2309&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-          <div style={{backgroundColor: 'rgba(30,30,30,0.7)', marginTop: '23px', height: '90%', width: '85%', color: 'white', borderRadius: '5px', border: 'solid 3px white',backgroundImage:'url(https://techcrunch.com/wp-content/uploads/2015/10/vidya.jpg?w=1390&crop=1)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
+          <div style={{backgroundColor: 'rgba(30,30,30,0.9)', marginTop: '23px', height: '90%', width: '85%', color: 'white', borderRadius: '5px', border: 'solid 3px white',backgroundImage:'url(https://techcrunch.com/wp-content/uploads/2015/10/vidya.jpg?w=1390&crop=1)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
           </div>
           </div>
         </div>
 
         <div className="game-main-div-rev">
           <div className="game-div-three" style={{backgroundImage:'url(https://images.unsplash.com/photo-1506808547685-e2ba962ded60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-          <div style={{backgroundColor: 'rgba(30,30,30,0.7)',marginTop: '39px', height:"90%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white', backgroundImage:'url(https://image.freepik.com/free-vector/neon-gamer-illustration-flat-design_52683-14179.jpg)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
+          <div style={{backgroundColor: 'rgba(30,30,30,0.9)',marginTop: '39px', height:"90%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white', backgroundImage:'url(https://image.freepik.com/free-vector/neon-gamer-illustration-flat-design_52683-14179.jpg)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
           </div>
           </div>
           <div className="game-double-div">
             <div className="game-div-one" style={{backgroundImage:'url(https://images.unsplash.com/photo-1517281749396-564b95a206c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)',marginTop: '19px', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.9)',marginTop: '19px', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
             <p style={{color: 'white', padding: '20px'}}>Construct 2</p>
             <p style={{color: 'white', padding: '20px'}}>Quest</p>
             <p style={{color: 'white', padding: '20px'}}>Unity</p>
@@ -44,7 +44,7 @@ export default class Other2 extends Component {
             </div>
             </div>
             <div className="game-div-two" style={{backgroundImage:'url(https://images.unsplash.com/photo-1559703248-dcaaec9fab78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)',height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.9)',height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <p style={{color: 'white', padding: '20px'}}>PC technologies</p>
             </div>
             </div>
@@ -54,7 +54,7 @@ export default class Other2 extends Component {
         <div className="game-main-div">
           <div className="game-double-div">
             <div className="game-div-one" style={{backgroundImage:'url(https://images.unsplash.com/photo-1573487903232-46df3657c783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.9)', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
             <p style={{color: 'white', padding: '20px'}}>OGRE</p>
             <p style={{color: 'white', padding: '20px'}}>Unreal Engine</p>
             <p style={{color: 'white', padding: '20px'}}>Unity</p>
@@ -62,13 +62,13 @@ export default class Other2 extends Component {
             </div>
             </div>
             <div className="game-div-two" style={{backgroundImage:'url(https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.9)', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <p style={{color: 'white', padding: '20px'}}>Console Devices</p>
             </div>
             </div>
           </div>
           <div className="game-div-three" style={{backgroundImage:'url(https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-          <div style={{backgroundColor: 'rgba(30,30,30,0.7)', marginTop: "19px", height:"90%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white', backgroundImage:'url(https://image.freepik.com/free-vector/futuristic-background-computer-gamer_23-2148207098.jpg)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
+          <div style={{backgroundColor: 'rgba(30,30,30,0.9)', marginTop: "19px", height:"90%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white', backgroundImage:'url(https://image.freepik.com/free-vector/futuristic-background-computer-gamer_23-2148207098.jpg)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
 
           </div>
           </div>
@@ -76,13 +76,13 @@ export default class Other2 extends Component {
 
            <div className="game-main-div-rev">
           <div className="game-div-three" style={{backgroundImage:'url(https://images.unsplash.com/photo-1506808547685-e2ba962ded60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-          <div style={{backgroundColor: 'rgba(30,30,30,0.7)', marginTop: "19px", height:"90%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white',backgroundImage:'url(https://image.freepik.com/free-vector/coloured-retro-video-games_1165-3.jpg)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
+          <div style={{backgroundColor: 'rgba(30,30,30,0.9)', marginTop: "19px", height:"90%",width:'85%', color: 'white', borderRadius: '5px', border: 'solid 3px white',backgroundImage:'url(https://image.freepik.com/free-vector/coloured-retro-video-games_1165-3.jpg)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
           
           </div>
           </div>
           <div className="game-double-div">
             <div className="game-div-one" style={{backgroundImage:'url(https://images.unsplash.com/photo-1517281749396-564b95a206c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)',  height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.9)',  height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white'}}>
             <p style={{color: 'white', padding: '20px'}}>Unity</p>
             <p style={{color: 'white', padding: '20px'}}>Construct 2</p>
             <p style={{color: 'white', padding: '20px'}}>Game Salad</p>
@@ -91,7 +91,7 @@ export default class Other2 extends Component {
             </div>
             </div>
             <div className="game-div-two" style={{backgroundImage:'url(https://images.unsplash.com/photo-1559703248-dcaaec9fab78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80)', backgroundPosition: 'center',backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div style={{backgroundColor: 'rgba(30,30,30,0.7)', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{backgroundColor: 'rgba(30,30,30,0.9)', height:"80%",width:'80%', color: 'white', borderRadius: '5px', border: 'solid 3px white',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <p style={{color: 'white', padding: '20px'}}>Hand-held Devices</p>
             </div>
             </div>
